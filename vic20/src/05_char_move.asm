9d0b7ab9bfb3843794f28e8d1b5e7d5f47ca9586
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2025-06-29 23:33:54 +0100

    move character one position up

--- vic20/src/05_char_move.asm
+++ vic20/src/05_char_move.asm
@@ -2,27 +2,59 @@
 .label SCNKEY=$FF9F
 .label GETIN=$FFE4
 .label CHROUT=$FFD2
-.label CLRSCR=$93
-.label PLAYER=83
 .label CHRSET=$9005
 .label RASTER_CNT = $9004
-.label RED=2
+.label SCREEN_RAM_START=$1E00
+.label COLOR_RAM_START=$9600
+
+.const RED=2
+.const PLAYER=83
+.const SPACE=32
+.const CLRSCR=$93
+.const SCREEN_RAM_OFFSET=$00E6
+.const KEY_UP=$91
+.const KEY_ENTER=13
 
 //5. **Single Character Move** - Display a character that can be moved with keyboard input (WASD or cursor keys)
 start:
-	lda #CLRSCR
-	jsr CHROUT 
+	jsr clear_scr
 
 	lda #RED
-	sta $96E6
+	sta COLOR_RAM_START+SCREEN_RAM_OFFSET
 
 	lda #PLAYER
-	sta $1EE6
+	sta SCREEN_RAM_START+SCREEN_RAM_OFFSET
 
 getchr: jsr SCNKEY
 		jsr GETIN
-		cmp #13
-		bne getchr
+		cmp #KEY_UP
+		beq moveup
+		cmp #KEY_ENTER
+		beq game_over
+		jmp getchr
 
-jmp *
+moveup:	lda #RED
+		sta COLOR_RAM_START+SCREEN_RAM_OFFSET-22
+		lda #PLAYER
+		sta SCREEN_RAM_START+SCREEN_RAM_OFFSET-22
+		jmp getchr
 
+game_over: 	ldx #0
+
+			jsr clear_scr
+
+			loop_char: 	lda message,X
+						cmp #0
+						beq end
+						jsr CHROUT
+
+						inx
+						jmp loop_char
+clear_scr: 	lda #CLRSCR
+			jsr CHROUT
+			rts
+
+message: 	.text "THANKS FOR PLAYING!"
+			.byte 0
+end:
+	rts
